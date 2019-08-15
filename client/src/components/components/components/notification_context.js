2fbaa3bf505d3f107dc6fd9f09ff876565ea18f4
limonsifat <Limon.Limon@MyGeorgian.ca>
2019-08-15 19:08:17 -0400

    again

--- client/src/components/components/components/notification_context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-const NotificationContext = createContext();
-
-export const NotificationProvider = NotificationContext.Provider;
-export const NotificationConsumer = NotificationContext.Consumer;
-
-export default NotificationContext;
--- client/src/components/components/components/partials/notification.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Notification() {
-  const contextType = NotifyContext;
-}
-
-export default Notification;
